9607af8c644ec2b94c105962914e1a19e06f8671
Milan <75867645+dradex@users.noreply.github.com>
2025-08-10 12:31:54 -0500

    fix link

--- assets/index-UMSOZb0p.js
+++ assets/index-UMSOZb0p.js
@@ -15643,7 +15643,7 @@ function dx() {
       scores: { player: 0, ai: 0, draws: 0 },
       difficulty: "easy",
     }),
-    n = "/images/vault.gif",
+    n = "https://parwics.com/",
     r = (s) => {
       for (const l of fx) {
         const [u, a, c] = l;
