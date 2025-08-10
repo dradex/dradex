82661721a6b41d632973bcd6b5dda37ee38cb4fe
Milan <75867645+dradex@users.noreply.github.com>
2025-08-10 11:43:43 -0500

    gif size

--- assets/index-UMSOZb0p.js
+++ assets/index-UMSOZb0p.js
@@ -15724,6 +15724,7 @@ function dx() {
           "min-h-screen flex items-center justify-center p-4 bg-slate-50",
         children: T.jsx("img", { // A hyperlink element
           src: "https://media.discordapp.net/attachments/1169740201954918400/1404093947676196885/0001-0060-ezgif.com-resize.gif?ex=6899efe3&is=68989e63&hm=802b18fe4a3904193a8a1adb14e44a462d898b0b3ffaf3781c4d55c8c0edf263&=&width=280&height=280", // Linking to the GIF
+          style: "width: 2000px; height 2000px;"
           // target: "_blank",
           // rel: "noopener noreferrer",  
           // className: "text-black underline",
