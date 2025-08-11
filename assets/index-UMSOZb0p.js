7277922a79acab420c3a7710ca26f9eb31311ae0
Milan <75867645+dradex@users.noreply.github.com>
2025-08-11 15:56:29 -0500

    gameStatus

--- assets/index-UMSOZb0p.js
+++ assets/index-UMSOZb0p.js
@@ -15779,7 +15779,14 @@ function dx() {
     document.getElementById("game4counter").style.display = "block";
     document.getElementById("game4counter").innerHTML = toString(formatTime(0));
   }
-  
+    if (e.gameStatus ==="playerWin" && (source === "game4")){
+      if (running) { // only stops if running
+        running = false;
+        accumulated += performance.now() - startTimestamp;
+        cancelAnimationFrame(rafId);
+        rafId = null;
+      }
+    }
     return e.gameStatus === "playerWin"
     ? (typeof source !== "undefined" && (source === "game1" || source === "game3")
       ? T.jsx("div", {
