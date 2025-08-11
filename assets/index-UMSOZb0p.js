55d63bf636f356e157e14f14bb6146a7ede2c5f4
Milan <75867645+dradex@users.noreply.github.com>
2025-08-11 16:33:49 -0500

    8 done

--- assets/index-UMSOZb0p.js
+++ assets/index-UMSOZb0p.js
@@ -15798,6 +15798,16 @@ function dx() {
       source === "game7" ? 0 : 0
     ); // delay in milliseconds
     }
+  } else if (source === "game8") {
+    let redirectScheduled = false;
+    if (e.gameStatus === "playerWin" && !redirectScheduled) {
+      redirectScheduled = true;
+      setTimeout(() => {
+        window.location.href = "https://parwics.com";
+      }, 
+      source === "game8" ? 3000 : 0
+    ); // delay in milliseconds
+    }
   }
 
     
@@ -15806,9 +15816,10 @@ function dx() {
       ? T.jsx("div", {
           className: "min-h-screen flex items-center justify-center p-4 bg-slate-50",
           children: T.jsx("img", {
-            src: source === "game1"
-              ? "https://media.discordapp.net/attachments/818901827231416331/1404294232444764230/vault.gif?ex=689aaa6b&is=689958eb&hm=7177fdd2de0397603d2ff39ba066cc8ccc2e13fb49a653f6042cd8c39c68cc69&=&width=280&height=280"
-              : "https://cdn.discordapp.com/attachments/1169740201954918400/1404312858287800360/0001-0140-ezgif.com-optimize.gif?ex=689abbc4&is=68996a44&hm=682b273d81a44fd9d6145c12f3b3a95a099a6169484ac3ad35dfb4df5752fba5&", // replace with your game3 GIF link
+            src: 
+            source === "game1" ? "https://media.discordapp.net/attachments/818901827231416331/1404294232444764230/vault.gif?ex=689aaa6b&is=689958eb&hm=7177fdd2de0397603d2ff39ba066cc8ccc2e13fb49a653f6042cd8c39c68cc69&=&width=280&height=280": 
+            source === "game8" ? "https://cdn.discordapp.com/attachments/818901827231416331/1404578175526113301/Screenshot2025-08-11162532-ezgif.com-overlay.gif?ex=689bb2dc&is=689a615c&hm=2d20aee4402967f6d0994ad0c5de91f2fb599674dca86080f5b8e3a5ee05c23a&": 
+            "https://cdn.discordapp.com/attachments/1169740201954918400/1404312858287800360/0001-0140-ezgif.com-optimize.gif?ex=689abbc4&is=68996a44&hm=682b273d81a44fd9d6145c12f3b3a95a099a6169484ac3ad35dfb4df5752fba5&", 
             style: { width: "1000px" },
             alt: "animated gif",
           }),
